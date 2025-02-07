420929619d09ba891963bc5a63f353586b291612
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-02-07 23:50:22 +0500

    add project

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './style.css';
-import shakeel from '../Images/shakeelpic.jpg'
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import { FaFacebookF, FaInstagram , FaLinkedinIn, FaReact, FaBootstrap, FaNodeJs, FaWordpress } from "react-icons/fa";
 
@@ -18,26 +17,26 @@ const Home = () => {
     return (
         <div className='container'>
             <div className='row'>
-                <div id='home' className='col-12 col-md-10 mx-auto py-md-5 pt-2 pb-4 d-flex flex-column gap-4 flex-md-row'>
-                    <div className='col-12 col-md-7 mt-md-4 d-flex flex-column gap-md-4 order-1 order-md-0'>
+                <div id='home' className='col-12 col-md-10 mx-auto my-3 py-md-5 pt-2 pb-4 d-flex flex-column gap-4 flex-md-row'>
+                    <div className='col-12 mt-md-4 d-flex flex-column gap-md-4 order-1 order-md-0'>
                         <div className='d-flex flex-column gap-1'>
-                            <h6 className='font-weight-normal text-white'>WELCOME TO MY WORLD</h6>
-                            <h1 className='font-weight-bold text-white'>Hi, I'm <span className='textRed'>Muhammad Shakeel</span></h1>
-                            <h3 className='font-weight-bold text-white'>a <span>{text}</span><Cursor cursorBlinking="false" cursorStyle="|" cursorColor='#ff014f' /></h3>
-                            <p>I use animation as a third dimension by which to simplify experiences and kuiding throw each and every interaction. I'm not adding motion just to spruce things up, but doing it in ways that.</p>
+                            <h6 className='font-weight-normal text-white text-center'>WELCOME TO MY WORLD</h6>
+                            <h1 className='font-weight-bold text-white text-center'>Hi, I'm <span className='textRed'>Muhammad Shakeel</span></h1>
+                            <h3 className='font-weight-bold text-white text-center'>a <span>{text}</span><Cursor cursorBlinking="false" cursorStyle="|" cursorColor='#ff014f' /></h3>
+                            <p className='text-center '>I use animation as a third dimension by which to simplify experiences and kuiding throw each and every interaction. I'm not adding motion just to spruce things up, but doing it in ways that.</p>
                         </div>
-                        <div className='d-flex justify-content-between'>
+                        <div className='col-12 col-md-7 mx-auto d-flex justify-content-between'>
                             <div>
-                                <h6 className='font-weight-normal py-1'>FIND ME IN</h6>
-                                <div className='d-flex gap-2 align-items-center text-center'>
+                                <h6 className='font-weight-normal py-1 text-center'>FIND ME IN</h6>
+                                <div className='d-flex gap-2 gap-md-4 align-items-center text-center'>
                                     <a href="https://web.facebook.com/profile.php?id=100083701840637" target="_blank" rel="noopener noreferrer" className="social-icon"> <FaFacebookF /> </a>
                                     <a href='https://www.instagram.com/shakeel.developer.pro' target='_blank' rel='noopener noreferrer' className='social-icon'><FaInstagram  /></a>
                                     <a href='https://pk.linkedin.com/in/muhammad-shakeel-b048b0301' target='_blank' rel='noopener noreferrer' className='social-icon'><FaLinkedinIn  /></a>
                                 </div>
                             </div>
                             <div>
-                                <h6 className='font-weight-normal py-1'>BEST SKILL ON</h6>
-                                <div className='d-flex gap-2 align-items-center text-center'>
+                                <h6 className='font-weight-normal py-1 text-center'>BEST SKILL ON</h6>
+                                <div className='d-flex gap-2 gap-md-4 align-items-center text-center'>
                                     <span className='social-icon' onClick={()=>window.location.href="/web-development"}><FaReact /></span>
                                     <span className='social-icon' onClick={()=>window.location.href="/web-development"}><FaBootstrap /></span>
                                     <span className='social-icon' onClick={()=>window.location.href="/web-development"}><FaNodeJs /></span>
@@ -45,9 +44,6 @@ const Home = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='col-12 col-md-5 mx-auto justify-content-center text-center align-items-center order-0 order-md-1'>
-                        <img src={shakeel} className='home-image' alt='Home' />
                     </div>
                 </div>
             </div>
--- src/Components/projects/ProjectsData.js
+++ src/Components/projects/ProjectsData.js
@@ -1,82 +1,135 @@
-import lottery from './ProjectImages/lottery.PNG';
-import lottery1 from './ProjectImages/lottery1.PNG';
-import lottery2 from './ProjectImages/lottery2.PNG';
-import lottery3 from './ProjectImages/lottery3.PNG';
-import lottery4 from './ProjectImages/lottery4.PNG';
-import lottery5 from './ProjectImages/lottery5.PNG';
+import lottery from "./ProjectImages/lottery.PNG";
+import lottery1 from "./ProjectImages/lottery1.PNG";
+import lottery2 from "./ProjectImages/lottery2.PNG";
+import lottery3 from "./ProjectImages/lottery3.PNG";
+import lottery4 from "./ProjectImages/lottery4.PNG";
+import lottery5 from "./ProjectImages/lottery5.PNG";
 
-import codes1 from './ProjectImages/codes1.PNG';
-import codes2 from './ProjectImages/codes2.PNG';
-import codes3 from './ProjectImages/codes3.PNG';
-import codes4 from './ProjectImages/codes4.PNG';
-import codes5 from './ProjectImages/codes5.PNG';
-import codes6 from './ProjectImages/codes6.PNG';
-import codes7 from './ProjectImages/codes7.PNG';
+import codes1 from "./ProjectImages/codes1.PNG";
+import codes2 from "./ProjectImages/codes2.PNG";
+import codes3 from "./ProjectImages/codes3.PNG";
+import codes4 from "./ProjectImages/codes4.PNG";
+import codes5 from "./ProjectImages/codes5.PNG";
+import codes6 from "./ProjectImages/codes6.PNG";
+import codes7 from "./ProjectImages/codes7.PNG";
 
-import spam1 from './ProjectImages/spam1.PNG';
-import spam2 from './ProjectImages/spam2.PNG';
-import spam3 from './ProjectImages/spam3.PNG';
+import spam1 from "./ProjectImages/spam1.PNG";
+import spam2 from "./ProjectImages/spam2.PNG";
+import spam3 from "./ProjectImages/spam3.PNG";
+
+import giverConsulting from './ProjectImages/giver.PNG';
+import giver1 from './ProjectImages/giver1.png';
+import giver2 from './ProjectImages/giver2.png';
+import giver3 from './ProjectImages/giver3.png';
+
 
 export const projects = [
-    {
-        id: 101,
-        title: "Lottery System",
-        category: "web",
-        image: lottery,
-        githubLink: "https://github.com/shakeel7521951/Lottery",
-        projectLink: "https://lottery-tes4.vercel.app",
-        description: "A web-based lottery system that allows users to participate in random draws and check results in real-time.",
-        features: [
-          "User Registration and Login",
-          "Random Number Generation for Lottery Draws",
-          "Real-Time Result Display",
-          "Responsive Design"
-        ],
-        technologies: ["React", "Node.js", "Express", "MongoDB", "CSS","BootStrap"],
-        additionalImages: [
-          lottery1,lottery2,lottery3,lottery,lottery4,lottery5
-        ]
-      },
+  {
+    id: 101,
+    title: "Lottery System",
+    category: "web",
+    image: lottery,
+    githubLink: "https://github.com/shakeel7521951/Lottery",
+    projectLink: "https://lottery-tes4.vercel.app",
+    description:
+      "A web-based lottery system that allows users to participate in random draws and check results in real-time.",
+    features: [
+      "User Registration and Login",
+      "Random Number Generation for Lottery Draws",
+      "Real-Time Result Display",
+      "Responsive Design",
+    ],
+    technologies: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "CSS",
+      "BootStrap",
+    ],
+    additionalImages: [
+      lottery1,
+      lottery2,
+      lottery3,
+      lottery,
+      lottery4,
+      lottery5,
+    ],
+  },
 
-      {
-        id: 102,
-        title: "Code's Thinker",
-        category: "web",
-        image: codes1,
-        projectLink: "https://codesthinker.com",
-        description: "Code's Thinker is a software house that offers educational services in areas like web development, app development, and more. It also provides custom project development for clients, ensuring solutions tailored to various needs.",
-        features: [
-          "Professional Training in Web Development and App Development",
-          "Custom Project Development Services",
-          "Comprehensive Learning Resources and Tutorials",
-          "Project Consultation and Mentorship",
-          "Responsive and User-Friendly Design"
-        ],
-        technologies: ["React", "Node.js", "Express", "MongoDB", "CSS", "Tailwind"],
-        additionalImages: [
-          codes2, codes3, codes4, codes5, codes6, codes7
-        ]
-      },
+  {
+    id: 102,
+    title: "The Giver Consulting ",
+    category: "web",
+    image: giverConsulting,
+    githubLink: "",
+    projectLink: "https://thegiversconsulting.com",
+    description:
+      "A dynamic and innovative company offering expert consultancy, overseas employment, IT services, general order supplies, import/export, and mining solutions. With a global vision and client-centric approach, we empower businesses and individuals through tailored, sustainable, and high-quality services.",
+    features: [
+      "Comprehensive Business Solutions",
+      "Overseas Employment Services",
+      "IT-Enabled Services",
+      "General Order Supplies",
+      "Import & Export",
+      "Mining & Mineral Resources",
+      "Global Reach & Network",
+      "Sustainability & Innovation ",
+    ],
+    technologies: [
+      "React",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "CSS",
+      "Tailwind",
+    ],
+    additionalImages: [giver1, giver2, giver3],
+  },
 
-      {
-        id: 103,
-        title: "Email/SMS Spam Classifier",
-        category: "dataScience",
-        image: spam1,
-        githubLink: "https://github.com/shakeel7521951/sms-spam-detection",
-        projectLink: "https://sms-spam-detection-by-shakeel.streamlit.app",
-        description: "A machine learning model that classifies messages as spam or not spam based on content analysis.",
-        features: [
-          "Email and SMS spam detection",
-          "Text preprocessing and feature extraction",
-          "High accuracy with machine learning algorithms",
-          "User-friendly interface for input and output",
-          "Real-time classification with interactive results"
-        ],
-        technologies: ["NumPy", "Pandas", "Matplotlib", "Seaborn", "scikit-learn", "Natural Language Processing"],
-        additionalImages: [
-          spam1, spam2, spam3
-        ]
-      }      
-      
-]
+  {
+    id: 103,
+    title: "Code's Thinker",
+    category: "web",
+    image: codes1,
+    projectLink: "https://codesthinker.com",
+    description:
+      "Code's Thinker is a software house that offers educational services in areas like web development, app development, and more. It also provides custom project development for clients, ensuring solutions tailored to various needs.",
+    features: [
+      "Professional Training in Web Development and App Development",
+      "Custom Project Development Services",
+      "Comprehensive Learning Resources and Tutorials",
+      "Project Consultation and Mentorship",
+      "Responsive and User-Friendly Design",
+    ],
+    technologies: ["React", "Node.js", "Express", "MongoDB", "CSS", "Tailwind"],
+    additionalImages: [codes2, codes3, codes4, codes5, codes6, codes7],
+  },
+
+  {
+    id: 104,
+    title: "Email/SMS Spam Classifier",
+    category: "dataScience",
+    image: spam1,
+    githubLink: "https://github.com/shakeel7521951/sms-spam-detection",
+    projectLink: "https://sms-spam-detection-by-shakeel.streamlit.app",
+    description:
+      "A machine learning model that classifies messages as spam or not spam based on content analysis.",
+    features: [
+      "Email and SMS spam detection",
+      "Text preprocessing and feature extraction",
+      "High accuracy with machine learning algorithms",
+      "User-friendly interface for input and output",
+      "Real-time classification with interactive results",
+    ],
+    technologies: [
+      "NumPy",
+      "Pandas",
+      "Matplotlib",
+      "Seaborn",
+      "scikit-learn",
+      "Natural Language Processing",
+    ],
+    additionalImages: [spam1, spam2, spam3],
+  },
+];
